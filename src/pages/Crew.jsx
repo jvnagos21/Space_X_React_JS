0eb7a9ffe68a083e27276615e5c02097dfbb1d17
jvnagos21 <jvmeira98@gmail.com>
2023-09-25 23:23:47 -0300

    feat/ finished crew hub page

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { Loading } from "../components";
 
 export default function Crew() {
+  const [crew, setCrew] = useState([]);
+
+  useEffect(() => {
+    const fetchCrew = async () => {
+      const res = await fetch("https://api.spacexdata.com/v4/crew");
+      const data = await res.json();
+      setCrew(data);
+    };
+
+    fetchCrew();
+  }, []);
+
   return (
-    <div>Crew</div>
-  )
+    <>
+      {!crew ? (
+        <Loading />
+      ) : (
+        <section className="py-32">
+          <h1 className="heading text-center mb-10">Crew</h1>
+          <div className="max-width grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 px-5">
+            {crew.map(({ id, name, image }) => (
+              <article className="relative">
+                <img
+                  src={image}
+                  alt={name}
+                  loading="lazy"
+                  className="h-96 w-full object-cover"
+                />
+                <h2 className="absolute bottom-5 left-5 font-bold text-white text-lg tracking-wide">
+                  {name}
+                </h2>
+              </article>
+            ))}
+          </div>
+        </section>
+      )}
+    </>
+  );
 }
