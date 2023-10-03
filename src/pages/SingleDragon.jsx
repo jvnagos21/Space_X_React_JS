2cb395ef0cefb184f483f98d53c00c094a0790ff
jvnagos21 <jvmeira98@gmail.com>
2023-10-02 22:08:46 -0300

    feat/ almost finished dragon page style

--- src/pages/SingleDragon.jsx
+++ src/pages/SingleDragon.jsx
@@ -21,17 +21,23 @@ export default function SingleDragon() {
       {!singleDragon ? (
         <Loading />
       ) : (
-        <section className="py-32 max-width">
-          <article>
-            <h1>{singleDragon.name}</h1>
-            <h2>First Flight Date: {singleDragon.first_flight}</h2>
-            <div>
-              <ul>
+        <section className="py-32 max-width flex flex-col-reverse md:grid md:grid-cols-2 md:gap-10">
+          <article className="mt-8 md:mt-0">
+            <h1 className="heading mb-8">{singleDragon.name}</h1>
+            <h2 className="font-bold opacity-80 text-lg lg:text-2xl mb-10 text-white">
+              First Flight Date: {singleDragon.first_flight}
+            </h2>
+            <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
+              <ul className="text-sm text-white opacity-75 capitalize flex flex-col items-start justify-start gap-3">
                 <li>Type: {singleDragon.type}</li>
                 <li>Crew: {singleDragon.crew_capacity}</li>
                 <li>Dry Mass: {singleDragon.dry_mass_lb}</li>
-                <li>Active: {singleDragon.active}</li>
-                <li>
+                {singleDragon.active ? (
+                  <li className="text-emerald-500">Active</li>
+                ) : (
+                  <li className="text-rose-500">inactive</li>
+                )}
+                <li className="mt-3">
                   <a
                     href={singleDragon.wikipedia}
                     target="_blank"
@@ -42,74 +48,91 @@ export default function SingleDragon() {
                   </a>
                 </li>
               </ul>
-              <ul>
-                <h3>Heat Shield details</h3>
-                <li>Material: {singleDragon.heat_shield.material}</li>
-                <li>Size: {singleDragon.heat_shield.size_feet}</li>
-                <li>
+              <ul className="bg-neutral-900 text-white text-sm opacity-75 p-3 rounded">
+                <h3 className="font-bold opacity-100 text-lg uppercase mb-3">
+                  Heat Shield details
+                </h3>
+                <li className="mb-3">
+                  Material: {singleDragon.heat_shield.material}
+                </li>
+                <li className="mb-3">
+                  Size: {singleDragon.heat_shield.size_meters}m
+                </li>
+                <li className="mb-3">
                   Temperature: {singleDragon.heat_shield.temp_degrees} Degrees
                 </li>
                 <li>Dev Partner: {singleDragon.heat_shield.dev_partner}</li>
               </ul>
             </div>
-            <p>{singleDragon.description}</p>
-            <div>
+            <p className="opacity-75 text-white mt-5 mb-8">
+              {singleDragon.description}
+            </p>
+            <div className="opacity-75 text-white text-sm">
               {/* meters*/}
-              <ul>
+              <ul className="grid grid-cols-2 gap-8">
                 <li>
-                  Launch Payload Mass: {singleDragon.launch_payload_mass.kg}{" "}
+                  Launch Payload Mass: {singleDragon.launch_payload_mass.kg} kg
                 </li>
                 <li>
-                  Return Payload Mass:{singleDragon.return_payload_mass.kg}
+                  Return Payload Mass:{singleDragon.return_payload_mass.kg}kg
                 </li>
                 <li>
                   Pressurized Capsule Payload Volume:{" "}
                   {singleDragon.pressurized_capsule.payload_volume.cubic_meters}
+                  m<sup>3</sup>
                 </li>
-                <li>Height With Trunk: {singleDragon.height_w_trunk.meters}</li>
+                <li>
+                  Height With Trunk: {singleDragon.height_w_trunk.meters}m
+                </li>
                 <li>
                   Launch Payload Volume:{" "}
-                  {singleDragon.launch_payload_vol.cubic_meters}
+                  {singleDragon.launch_payload_vol.cubic_meters}m<sup>3</sup>
                 </li>
                 <li>
                   Return Payload Volume:{" "}
                   {singleDragon.pressurized_capsule.payload_volume.cubic_meters}
+                  m<sup>3</sup>
                 </li>
                 <li>
-                  Trunk Volume: {singleDragon.trunk.trunk_volume.cubic_meters}
+                  Trunk Volume: {singleDragon.trunk.trunk_volume.cubic_meters}m
+                  <sup>3</sup>
                 </li>
-                <li>Diameter: {singleDragon.diameter.meters}</li>
+                <li>Diameter: {singleDragon.diameter.meters}m</li>
               </ul>
 
               {/*IMPERIAL*/}
-              <ul>
+              <ul className="grid grid-cols-2 gap-8">
                 <li>
-                  Launch Payload Mass: {singleDragon.launch_payload_mass.lb}{" "}
+                  Launch Payload Mass: {singleDragon.launch_payload_mass.lb} lb
                 </li>
                 <li>
-                  Return Payload Mass:{singleDragon.return_payload_mass.lb}
+                  Return Payload Mass:{singleDragon.return_payload_mass.lb}lb
                 </li>
                 <li>
                   Pressurized Capsule Payload Volume:{" "}
-                  {singleDragon.pressurized_capsule.payload_volume.cubic_feet}
+                  {singleDragon.pressurized_capsule.payload_volume.cubic_feet}ft
+                  <sup>3</sup>
                 </li>
-                <li>Height With Trunk: {singleDragon.height_w_trunk.feet}</li>
+                <li>Height With Trunk: {singleDragon.height_w_trunk.feet}ft</li>
                 <li>
                   Launch Payload Volume:{" "}
-                  {singleDragon.launch_payload_vol.cubic_feet}
+                  {singleDragon.launch_payload_vol.cubic_feet}ft<sup>3</sup>
                 </li>
                 <li>
                   Return Payload Volume:{" "}
-                  {singleDragon.return_payload_vol.cubic_feet}
+                  {singleDragon.return_payload_vol.cubic_feet}ft<sup>3</sup>
                 </li>
                 <li>
-                  Trunk Volume: {singleDragon.trunk.trunk_volume.cubic_feet}
+                  Trunk Volume: {singleDragon.trunk.trunk_volume.cubic_feet}ft
+                  <sup>3</sup>
                 </li>
-                <li>Diameter: {singleDragon.diameter.feet}</li>
+                <li>Diameter: {singleDragon.diameter.feet}lb</li>
               </ul>
             </div>
           </article>
-          <img src={singleDragon.flickr_images[0]} alt={singleDragon.name} />
+          <article>
+            <img src={singleDragon.flickr_images[0]} alt={singleDragon.name} />
+          </article>
         </section>
       )}
     </>
