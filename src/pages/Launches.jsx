3da0842def99cbf7958f9aa3956be09707bc1779
jvnagos21 <jvmeira98@gmail.com>
2023-10-16 23:48:28 -0300

    feat/ finished Launches page

--- src/pages/Launches.jsx
+++ src/pages/Launches.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import useFetch from "../hooks/useFetch";
 import { Loading } from "../components";
+import { Link } from "react-router-dom";
 
 export default function Launches() {
   const [data] = useFetch("https://api.spacexdata.com/v4/launches");
@@ -12,6 +13,29 @@ export default function Launches() {
       ) : (
         <section className="py-32 max-width">
           <h1 className="heading text-center mb-10">Launches</h1>
+          <div className="max-width grid grid-cols-1 gap-5 md:grid-cols-2 px-5">
+            {data.map(({ id, name, links, details }) => (
+              <Link to={`/launches/${id}`} key={id} className="p-5 bg-zinc-900">
+                {links.patch.large ? (
+                  <img src={links.patch.large} alt={name} />
+                ) : (
+                  <img
+                    src="https://images2.imgbox.com/5b/02/QcxHUb5V_o.png"
+                    alt=""
+                  />
+                )}
+                <h2 className="font-bold text-white mt-5 mb-3 text-xl">
+                  {name}
+                </h2>
+                {details && (
+                  <p className="text-white opacity-75">{`${details.substring(
+                    0,
+                    50
+                  )}...`}</p>
+                )}
+              </Link>
+            ))}
+          </div>
         </section>
       )}
     </>
