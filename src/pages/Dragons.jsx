46d23fee40fa72cf038fdd117fda9baad40aabed
jvmeira23 <jvmeira98@gmail.com>
2024-01-22 14:27:27 -0300

    udpate/ dragons page date updated

--- src/pages/Dragons.jsx
+++ src/pages/Dragons.jsx
@@ -1,28 +1,18 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Loading } from "../components";
+import useFetch from "../hooks/useFetch";
 
 export default function Dragons() {
-  const [dragons, setDragons] = useState([]);
-
-  useEffect(() => {
-    const fetchDragons = async () => {
-      const res = await fetch("https://api.spacexdata.com/v4/dragons");
-      const data = await res.json();
-      setDragons(data);
-    };
-
-    fetchDragons();
-  }, []);
+  const [data] = useFetch("https://api.spacexdata.com/v4/dragons");
   return (
     <>
-      {!dragons ? (
+      {!data ? (
         <Loading />
       ) : (
         <section className="py-32">
           <h1 className="heading text-center mb-10">Dragons</h1>
           <div className="max-width grid grid-cols-1 gap-5 md:grid-cols-2 px-5">
-            {dragons.map(({ id, name, flickr_images, description }) => (
+            {data.map(({ id, name, flickr_images, description }) => (
               <Link to={`/dragons/${id}`} key={id}>
                 <article>
                   <img
