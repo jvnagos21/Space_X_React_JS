7214ed4ec75006a1d4c520a64569bff1b6f42a93
jvmeira23 <jvmeira98@gmail.com>
2024-01-22 14:21:06 -0300

    fix/ fix loading in dragons page

--- src/pages/Dragons.jsx
+++ src/pages/Dragons.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { Loading } from "../components";
 
 export default function Dragons() {
   const [dragons, setDragons] = useState([]);
@@ -15,34 +16,38 @@ export default function Dragons() {
   }, []);
   return (
     <>
-      <section className="py-32">
-        <h1 className="heading text-center mb-10">Dragons</h1>
-        <div className="max-width grid grid-cols-1 gap-5 md:grid-cols-2 px-5">
-          {dragons.map(({ id, name, flickr_images, description }) => (
-            <Link to={`/dragons/${id}`} key={id}>
-              <article>
-                <img
-                  src={flickr_images[0]}
-                  alt={name}
-                  className="h-96 object-cover"
-                />
-                <div className="bg-zinc-900 p-5">
-                  <h2 className="text-white text-lg mb-3 font-bold tracking-wider">
-                    {name}
-                  </h2>
-                  <p className="text-white opacity-75 mb-8">{`${description.substring(
-                    0,
-                    200
-                  )}...`}</p>
-                  <Link to={`/dragons/${id}`} className="btn">
-                    Read more &rarr;
-                  </Link>
-                </div>
-              </article>
-            </Link>
-          ))}
-        </div>
-      </section>
+      {!dragons ? (
+        <Loading />
+      ) : (
+        <section className="py-32">
+          <h1 className="heading text-center mb-10">Dragons</h1>
+          <div className="max-width grid grid-cols-1 gap-5 md:grid-cols-2 px-5">
+            {dragons.map(({ id, name, flickr_images, description }) => (
+              <Link to={`/dragons/${id}`} key={id}>
+                <article>
+                  <img
+                    src={flickr_images[0]}
+                    alt={name}
+                    className="h-96 object-cover"
+                  />
+                  <div className="bg-zinc-900 p-5">
+                    <h2 className="text-white text-lg mb-3 font-bold tracking-wider">
+                      {name}
+                    </h2>
+                    <p className="text-white opacity-75 mb-8">{`${description.substring(
+                      0,
+                      200
+                    )}...`}</p>
+                    <Link to={`/dragons/${id}`} className="btn">
+                      Read more &rarr;
+                    </Link>
+                  </div>
+                </article>
+              </Link>
+            ))}
+          </div>
+        </section>
+      )}
     </>
   );
 }
--- src/pages/Roadster.jsx
+++ src/pages/Roadster.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Roadster() {
-  return <div>Roadster</div>;
+  return (
+    <>
+      <section className="py-32 max-width">
+        <h1 className="heading text-center mb-10">
+          Elon Musk's Tesla Roadster
+        </h1>
+        <div></div>
+      </section>
+    </>
+  );
 }
